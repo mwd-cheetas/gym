014ee037b96d5d8ed361375df4e459152b8e588e
Syed Wasay Ali <Syed Wasay Ali@Wasay>
2017-04-29 02:22:45 +0500

    version 2

--- src/Trainer.java
+++ src/Trainer.java
@@ -1,13 +1,51 @@
 
 public abstract class Trainer {
-public Trainer(int age,String name,int weight,int experience)
+	protected int age;
+	protected String name;
+	protected int weight;
+	protected int experience;
+	protected String phone;
+	protected String email;
+public Trainer(int age,String name,int weight,int experience,String phone)
 {
+	this.age = age;
+	this.weight=weight;
+	this.experience=experience;
+	this.phone=phone;
+	this.name=name;
+	
+}
+public void cls(){
+	for(int i=0;i<=7;i++){
+		System.out.println("");
+	}
+	System.out.println("\t ********** \t");
+}
+public void viewtrainer()
+{
+	cls();
+	System.out.println("Name: "+ name+"\t Age: "+age);
+	System.out.println("Weight: "+ weight+"\t Experience: "+ experience);
 	
 }
 }
  class FitnessTrainer extends Trainer
 {
+		
+	public  FitnessTrainer(int age,String name,int weight,int experience,String phone)
+	{
+		super(age,name,weight,experience,phone);
+	}
+	
 }
  class MassGain extends Trainer
 {
+	 public  MassGain(int age,String name,int weight,int experience,String phone)
+		{
+			super(age,name,weight,experience,phone);
+		}
+}
+class scheduling{
+	
+	
 }
