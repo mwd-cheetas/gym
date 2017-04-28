9e23ba34c117b1d277695b98c5f49326febd9c1d
Syed Wasay Ali <Syed Wasay Ali@Wasay>
2017-04-29 02:20:21 +0500

    version 1

---
+++ src/Trainee.java
@@ -0,0 +1,12 @@
+
+public abstract class Trainee {
+
+}
+class Regular extends Trainee
+{
+	
+}
+class Premium extends Trainee
+{
+	
+}
---
+++ src/Trainer.java
@@ -0,0 +1,13 @@
+
+public abstract class Trainer {
+public Trainer(int age,String name,int weight,int experience)
+{
+	
+}
+}
+ class FitnessTrainer extends Trainer
+{
+}
+ class MassGain extends Trainer
+{
+}
