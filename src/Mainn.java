97b8b7b3ea8faaa9d14a1c8692194975dcc06b85
Syed Wasay Ali <Syed Wasay Ali@Wasay>
2017-04-29 14:29:08 +0500

    version 8

--- src/Mainn.java
+++ src/Mainn.java
@@ -5,7 +5,7 @@ public class Mainn {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		/////////
-		//Serialization here
+		// Serialization here
 		/////////
 	}
   
