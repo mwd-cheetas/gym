aaf1058c18efef7acde23219d991839f08cd2f9f
Syed Wasay Ali <Syed Wasay Ali@Wasay>
2017-04-29 11:00:20 +0500

    version 6

--- src/Mainn.java
+++ src/Mainn.java
@@ -1,9 +1,10 @@
 
+
 public class Mainn {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 }
--- src/Trainee.java
+++ src/Trainee.java
@@ -15,7 +15,32 @@ protected int weight;
 protected String phone;
 protected String email;
 protected String pass = "123";
+/////////////
+public void viewplan()
+{
+     if(t!=null)
+	 System.out.println("Trainer Not Hired !!!");
+     else
+     System.out.println(t.dietplan);	
+
 }
+public void viewschedule()
+{
+	   if(t!=null)
+			 System.out.println("Trainer Not Hired !!!");
+		     else
+		     System.out.println(t.schedule);	
+}
+}
+/////////////////////////////////////
+
+
+
+
+
+
+
+//////////////////////////////////////////
 class Regular extends Trainee
 {
 	public Regular(String name,int age,int weight,String phone,String email)
@@ -23,6 +48,7 @@ class Regular extends Trainee
 		super(name,age,weight,phone,email);		
 	}
 }
+/////////////////////////////////////////
 class Premium extends Trainee
 {
 	public Premium(String name,int age,int weight,String phone,String email)
