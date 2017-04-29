64c43609fca48e68638f757a7349ee6f7e3de7ef
Syed Wasay Ali <Syed Wasay Ali@Wasay>
2017-04-29 12:26:59 +0500

    version 7

--- src/Register.java
+++ src/Register.java
@@ -1,4 +1,16 @@
 
 public class Register {
+	RegisterTrainer rr ;
+	RegisterTrainee re;
+public Register(){
+	rr= new RegisterTrainer();
+	re=new RegisterTrainee();
+}
 
+}
+class RegisterTrainer{
+	
+}
+class RegisterTrainee{
+	
 }
